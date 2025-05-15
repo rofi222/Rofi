8b90d74a9ce1abb09bea8ea9840d001341db625e
rofi222 <m.rofi0604@gmail.com>
2025-05-15 15:20:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Selamat datang di website informatika")
+st.image("View/IMG_2025-04-20-17-29-29-053.jpg", width=200)
+st.write("\n")
+st.subheader("Muhammad 'Mister' Rofi")
+st.write("RAWRR")
